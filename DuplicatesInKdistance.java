55d7e04b2c76cdef89ec927dbd1176f3977b564a
Ramyac185 <132195574+Ramyac185@users.noreply.github.com>
2023-05-19 13:47:30 +0530

    Update DuplicatesInKdistance.java

--- DuplicatesInKdistance.java
+++ DuplicatesInKdistance.java
@@ -9,20 +9,21 @@ public class DuplicatesInKdistance {
 			for (int i = 0; i < n; i++) {
 			      int j = i + 1;
 			      int distance = k;
-			      while (distance > 0 && j < n) {
-			        if (nums[i] == nums[j]) {
-			          return true;
-			        }
-			        j++;
-			        distance--;
+			      while ( j < n) {
+			    	  if(distance > 0)
+			    		  if (nums[i] == nums[j]) {
+			    			  return true;
+			    		  }
+			    	  j +=1;
+			    	  distance--;
 			      }
 			    }
 			    return false;
 			  }
 
 	public static void main(String[] args) {
-		int[] nums = {1,0,2,1};
-		int k = 2;
+		int[] nums = {1,0,1,1};
+		int k = 1;
 		System.out.println(checkDuplicates(nums,k));
 	}
 
